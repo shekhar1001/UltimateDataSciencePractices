59c29a34a8d4db2b798a8a04768e5fa101a8db88
Shekhar Ghimire <shekhar.ghimire1001@gmail.com>
2025-06-04 17:04:34 +0545

    Some Scipy practices from the book:Day6

---
+++ Scipy/ex4.28.py
@@ -0,0 +1,15 @@
+# Triangulation function
+# Delaunay triangulation algorithm
+import numpy as mynp
+import scipy.spatial as myscpy
+import matplotlib.pyplot as myplt
+myarray = mynp.array([[2,5],[3,5],[3,3],[2,2],
+[4,1]])
+myvar_simplices =myscpy.Delaunay(myarray).simplices
+myxcoordinate = myarray[:, 0]
+myycoordinate = myarray[:, 1]
+myplt.triplot(myxcoordinate, myycoordinate,
+myvar_simplices)
+myplt.scatter(myxcoordinate, myycoordinate,
+color='b')
+myplt.show()
